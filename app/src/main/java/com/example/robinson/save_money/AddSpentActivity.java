446ac5bf30f6d5c417656488ff2ab9b4b21837b9
Robinson D. S. Santos <robinsondssantos@gmail.com>
2017-03-22 13:49:40 -0300

    Toast was added

--- app/src/main/java/com/example/robinson/save_money/AddSpentActivity.java
+++ app/src/main/java/com/example/robinson/save_money/AddSpentActivity.java
@@ -2,6 +2,9 @@ package com.example.robinson.save_money;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by robinson on 22/03/17.
@@ -12,6 +15,17 @@ public class AddSpentActivity extends AppCompatActivity {
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_add_spent);
+
+        Button addButton = (Button)  findViewById(R.id.buttonAddSpent);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AddSpentActivity.this, "A spent was added", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
 }
